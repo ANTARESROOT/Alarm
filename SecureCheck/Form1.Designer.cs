9dfe64c3284ed64824941aaf77332a95f785f076
ANTARESROOT <48474689+ANTARESROOT@users.noreply.github.com>
2019-05-31 11:48:18 +0300

    Add files via upload

---
+++ SecureCheck/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SecureCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(504, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Включить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(597, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Проверить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "http://project/login.php";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ВСЁ В ПОРЯДКЕ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(684, 79);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "SecureCheck";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SecureCheck/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+
+namespace SecureCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //bool x = true;
+            
+            //while (x = true)
+            //{
+                
+            //}
+
+        }
+
+        public void check()
+        {
+            string Check = new System.Net.WebClient().DownloadString(textBox1.Text);
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\bsound.wav");
+
+            if (Check.Contains("System blocked"))
+            {
+                
+
+                player.Play();
+                
+            }
+            else
+            {
+                System.Threading.Thread.Sleep(1000);
+                player.Stop();
+                
+
+                //timer1.Start();
+            }
+        }
+        public void alarm()
+        {
+            cx:
+            string Check = new System.Net.WebClient().DownloadString(textBox1.Text);
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\bsound.wav");
+
+            bool x = true;
+            while (x)
+            
+            {
+                if (Check.Contains("System blocked"))
+                {
+                    Thread.Sleep(100);
+                    label1.ForeColor = Color.Red;
+                    label1.Text = "ТРЕВОГА!!!";
+                    player.Play();
+                    System.Threading.Thread.Sleep(20000);
+                    player.Stop();
+                    
+                    goto cx;
+
+                }
+            else
+                {
+                    label1.ForeColor = Color.LightGreen;
+                    label1.Text = "ВСЁ В ПОРЯДКЕ";
+                    System.Threading.Thread.Sleep(1000);
+                    goto cx;
+
+                
+                }
+            }
+           
+                
+               
+            
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+           
+            button1.Enabled = false;
+            Thread myThread = new Thread(alarm);
+            //alarm();
+            myThread.Start();
+            
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            
+            check();
+            button2.Enabled = true;
+        }
+    }
+}
